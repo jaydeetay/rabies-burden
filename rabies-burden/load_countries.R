d382a3b3e6df1ff7e9ddebf47099860e4a0aa84c
John Taylor <jaydeetay@users.noreply.github.com>
2016-05-01 18:17:30 -0400

    Made the intermediate outputs in the input panel.

--- rabies-burden/load_countries.R
+++
@@ -1,5 +0,0 @@
-# Loads the countries
-
-if (!exists("country_data")) {
-  country_data=read.csv("vcountry2.csv", row.names = 2)
-}
--- rabies-burden/server.R
+++ rabies-burden/server.R
@@ -8,14 +8,22 @@
 #
 
 library(shiny)
-source('load_countries.R')
+
+print("ShinyServer: Server start up")
+country_data = read.csv("vcountry2.csv", row.names = 2)
+# This data is calculated by burden_1.R - we will assume this a given for
+# now.
+pPEPcountry = read.csv("pPEPcountry.csv", row.names = 2)
+print("ShinyServer: sourcing burden model")
+source('burden_model.R')
+print("ShinyServer: Burden model sourced")
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-  print("New User")
+  print("ShinyServer: New User")
   output$countrySelector<-renderUI({selectInput("countryChoice", "Country", rownames(country_data))})
   output$country<-renderText({
-    print("Country changed")
+    print("ShinyServer: Country changed")
     as.character(country_data[input$countryChoice, "Country"])})
   
   output$continent<-renderText({
@@ -27,21 +35,20 @@ shinyServer(function(input, output) {
   output$population<-renderText({
     as.character(country_data[input$countryChoice, "Human_population_2010"])})
   
+  output$pPEP_table<-renderTable({t(pPEPcountry[input$countryChoice,])})
+  
 
   # Running the rabies model is expensive - we don't want to redo it automatically
   # on every input change.  Instead only run it when the button is pressed.
   doCalculation <- eventReactive(input$start,{
-    print("Sourcing burden_model")
-    source('burden_model.R')
+    print("ShinyServer: Sourcing burden_model")
+    return(calculate_burden(countryCode = input$countryChoice, provideProgress = TRUE))
   })
   
-  output$dummy <- renderText(withProgress(message="Calculating", {
-    print("Do calculation")
+  output$burden_table <- renderTable(withProgress(message = "Calculating", detail = "Please wait...", {
+    print("ShinyServer: Do calculation")
     doCalculation()
-    ""
   }))
   
-  
-  
-  print("End server call")
+  print("ShinyServer: End server call")
 })
